f837829b213b8ccb2aa0f71d6569ccf7edb27cb5
muhamadArdalepa <ardalepa7muhamad@gmail.com>
2023-06-22 14:59:00 +0700

    adding create new user method

--- src/models/users.js
+++ src/models/users.js
@@ -2,10 +2,17 @@ const dbPool = require('../config/database');
 
 const getAllUsers = () => {
     const SQLQuery = "SELECT * FROM users";
-
     return dbPool.execute(SQLQuery);
 }
 
+const createNewUser = (body) => {
+    const SQLQuery = `  INSERT INTO users 
+                        VALUE(null,'${body.role}','${body.name}','${body.alamat}','${body.telp}')`;
+    return dbPool.execute(SQLQuery);
+}
+
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    createNewUser
 }
